f308f0323e5d57916a82412f541a770b1ace6673
Sandro Almeida <sandrodotbuild@gmail.com>
2024-01-18 16:15:15 -0800

    testing authentication process

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
 import { Button } from "@nextui-org/react";
+import * as actions from "@/actions";
+import { auth } from "@/auth";
+import Profile from "@/components/profile";
 
-export default function Home() {
+export default async function Home() {
+    const session = await auth();
     return (
         <div>
-            <Button>NextUI Button</Button>
+            <form action={actions.signIn}>
+                <Button type="submit">Sign In</Button>
+            </form>
+            <form action={actions.signOut}>
+                <Button type="submit">Sign Out</Button>
+            </form>
+
+            {session?.user ? (
+                <div>{JSON.stringify(session.user)}</div>
+            ) : (
+                <div>Not Signed In</div>
+            )}
+
+            <Profile />
         </div>
     );
 }
