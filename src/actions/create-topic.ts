a0ca3d678b542b7c0b523c6c3903fac1cf70e58f
Sandro Almeida <sandrodotbuild@gmail.com>
2024-01-21 17:27:40 -0800

    implementing form validation with zod

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,5 +1,24 @@
 "use server";
 
-export async function createTopic() {
-    // TODO: revalidate the home page
+import { z } from "zod";
+
+const createTopicSchema = z.object({
+    name: z
+        .string()
+        .min(3)
+        .regex(/^[a-z-]+$/, {
+            message: "Must be lowercase letters or dashes without spaces",
+        }),
+    description: z.string().min(10),
+});
+
+export async function createTopic(formData: FormData) {
+    const result = createTopicSchema.safeParse({
+        name: formData.get("name"),
+        description: formData.get("description"),
+    });
+
+    if (!result.success) {
+        console.log(result.error.flatten().fieldErrors);
+    }
 }
